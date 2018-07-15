b582ff75d4ae2e25a73d33cae9398d8cfc643b75
Ian Mitchell <ianmitch90@gmail.com>
2018-07-15 08:49:39 -0500

    simple fixes on landing for mobile and desktop

--- src/Components/Pages/Landing/Landing.jsx
+++ src/Components/Pages/Landing/Landing.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import '../../../styles/css/Landing.css'
 import { Parallax, ParallaxLayer } from 'react-spring'
-import face from '../../../Assets/images/imgface-min.png'
 
-const Page = ({ offset, caption, first, second, third, fourth, gradient, onClick }) => (
+
+const Page = ({ offset, caption, first, second, third, fourth, fifth, sixth, gradient, onClick }) => (
   <React.Fragment>
     <Parallax.Layer offset={offset} speed={0.2} onClick={onClick}>
       <div className="slopeBegin" />
@@ -25,6 +25,8 @@ const Page = ({ offset, caption, first, second, third, fourth, gradient, onClick
         <p>{second}</p>
         <p>{third}</p>
         <p>{fourth}</p>
+        <p>{fifth}</p>
+        <p>{sixth}</p>
       </span>
     </Parallax.Layer>
    
@@ -38,29 +40,25 @@ export class Landing extends Component {
     return (
       <div className= 'Landing'>
       <div className= 'landingContainer'>
-      <Parallax ref={ref => (this.parallax = ref)} pages={2}>
-          <ParallaxLayer offset={0.2} speed={0.23}>
+          <Parallax className='heroBackground' ref={ref => (this.parallax = ref)} pages={2}>
+          <ParallaxLayer className='heroText' offset={0.1} speed={0} >
+              <ParallaxLayer offset={0.1} speed={4.5}>
                 <h1>Ian</h1>
-          </ParallaxLayer>
-          
-          <ParallaxLayer offset={0.5} speed={0.5}>
-                <h1>Mitchell</h1>
-          </ParallaxLayer>
-          <ParallaxLayer offset={0.9} speed={1}>
-                <div className="servingFace"/> 
-          </ParallaxLayer>
+              </ParallaxLayer>
 
+              <ParallaxLayer offset={0.3} speed={2}>
+                <h1>Mitchell</h1>
+              </ParallaxLayer>
+          </ParallaxLayer>
 
           <ParallaxLayer offset={1}>
-            <div className ='snapshotContainer'>
               <Parallax className="container" ref="parallax" pages={5} horizontal scrolling={true}>
                  <Page className="imgME" offset={0} gradient="pink" caption="Who I Am" first="Ian Mitchell" second="Developer" third="&" fourth="Designer" onClick={() => this.scroll(1)} />
                   <Page offset={1} gradient="teal" caption="What I Do" first="" second="Development" third="Education" fourth="Freelance" onClick={() => this.scroll(2)} />
                   <Page offset={2} gradient="tomato" caption="Web Development Specialties" first="Jr Full Stack Dev" second="JS/HTML/CSS/C#" third="Mongo/PostSQL" fourth="React/Redux/MERN"onClick={() => this.scroll(3)} />
-                  <Page offset={3} gradient="algae" caption="Education" first="University of Houston" second="Computer Science - Current" third="The Iron Yard" fourth="Fullstack Web - 2017" onClick={() => this.scroll(4)} />
+                <Page offset={3} gradient="algae" caption="Education" first="University" second="of Houston" third="Computer Science " fourth="- Current" fifth ="The Iron Yard" sixth="Full Stack - 2017"onClick={() => this.scroll(4)} />
                   <Page offset={4} gradient="lavender" caption="Contact Me" first="ContactFormButton" second="Linkedn" third="GitHub" fourth="DetailedInfo" onClick={() => this.scroll(0)} />
                   </Parallax>
-              </div>
           </ParallaxLayer>
       </Parallax>
       </div>
