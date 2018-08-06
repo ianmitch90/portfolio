caedb63bcffb60c58f52ab1f19a58f323a1621a7
Ian Mitchell <ianmitch90@gmail.com>
2018-08-06 11:16:42 -0500

    fixed visual issues with contact page, and included css notes on some of the views

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -3,8 +3,7 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
-import { Face, Home, Code, Forum, Public} from '@material-ui/icons/'
-
+import { Face, Home, Code, Forum, Public } from '@material-ui/icons/'
 
 const styles = theme => ({
   button: {
@@ -19,31 +18,27 @@ function OutlinedButtons(props) {
   const { classes } = props;
   return (
     <div className='navigation'>
-    
-      <Button component={Link} to='/' variant="extendedFab" color="primary" className={classes.button}>
+      <div className='navContainer'>
+      <Button component={Link} to='/'  color="secondary" className={classes.button}>
         <Home/>
-        Home
       </Button>
 
-      <Button component={Link} to='/profile' variant="extendedFab" color="primary" className={classes.button}>
+      <Button component={Link} to='/profile'  color="secondary" className={classes.button}>
         <Face/>
-        About Me
       </Button>
 
-      <Button component={Link} to='/developer' variant="extendedFab" color="primary" className={classes.button}>
+      <Button component={Link} to='/developer'  color="secondary" className={classes.button}>
         <Code/>
-        Developer
       </Button>
 
-      <Button component={Link} to='/contact' variant="extendedFab" color="primary" className={classes.button}>
+      <Button component={Link} to='/contact'  color="secondary" className={classes.button}>
         <Forum/>
-        Contact
       </Button>
 
-      <Button component={Link} to='/social' variant="extendedFab" color="primary" className={classes.button}>
+      <Button component={Link} to='/social'  color="secondary" className={classes.button}>
         <Public/>
-        Social
       </Button>
+      </div>
 
 
     </div>
--- src/Components/Pages/Contact/Contact.jsx
+++ src/Components/Pages/Contact/Contact.jsx
@@ -1,12 +1,47 @@
 import React, { Component } from 'react'
 import './contact.css'
+import Button from '@material-ui/core/Button';
+import { Mail , ChevronRight, CloudDownload, DateRange, AssignmentInd} from '@material-ui/icons/'
+
+
+
 
 export class Contact extends Component {
   render() {
     return (
       <div className='contactContainer'>
-      <h1>Stay In Touch!</h1>
+        <h1>Stay In Touch!</h1>
+        <div className='contactMain'>
+          <h2>Email:</h2>
+            <Button variant='fab' rel="noopener noreferrer" href= 'mailto:ianmitch90@gmail.com'>
+              <Mail/>
+            </Button>
+          <h2>Phone:</h2>
+            <p>(832) 704-6499</p>
+          <h2>Linkedn:</h2>
+          <Button variant='fab' color= 'inherit' className= 'goButtonGreen' target="_blank" rel="noopener noreferrer" href= 'https://www.linkedin.com/in/ianmitch90/'>
+              <ChevronRight/>
+            </Button>
+          <h2>GitHub:</h2>
+          <Button variant='fab' color= 'inherit' className= 'goButtonGreen' target="_blank" rel="noopener noreferrer" href= 'https://github.com/ianmitch90'>
+              <ChevronRight/>
+            </Button>
+
+          <div className='contactButtonsContainer'>
+            <Button variant='extendedFab' color='primary' className='contactButtons'>
+              <CloudDownload /> <p>Resume</p>
+            </Button>
+            <Button variant='extendedFab' color='pink' className='contactButtons'>
+              <DateRange /> <p>Schedule</p>
+            </Button>
+            <Button variant='extendedFab' color='secondary' className='contactButtons'>
+              <AssignmentInd /> <p>Hireing?</p>
+            </Button>
+          </div>
+        </div>
       </div>
+      
+
     )
   }
 }
