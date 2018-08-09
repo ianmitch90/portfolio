44c3acdc81f8b53613372e3f6574faa67e7827b6
Ian Mitchell <ianmitch90@gmail.com>
2018-08-09 00:50:38 -0500

    travis intergration

--- src/Components/Pages/Social/Social.jsx
+++ src/Components/Pages/Social/Social.jsx
@@ -8,8 +8,6 @@ export class Social extends Component {
           <div className='socialHeroImage'>
             <div className='socialHeroText'>
                 <h1>Follow <br/> My <br/> Journey!</h1>
-                
-                
             </div>
           </div>
         </div>
