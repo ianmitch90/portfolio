9b7b31068114c84e981eb0287ba69a161e51528a
Ian Mitchell <ianmitch90@gmail.com>
2018-08-12 07:50:36 -0500

    Finished on layout
    finished layout of the entire page now just working on how the projects section will show up

--- src/Components/Pages/Developer/devLanguages.jsx
+++ src/Components/Pages/Developer/devLanguages.jsx
@@ -1,12 +1,75 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import csharpIcon from '../../../Assets/devicons/csharp-line.svg'
+import cssIcon from '../../../Assets/devicons/css3-original.svg'
+import dockerIcon from '../../../Assets/devicons/docker-original.svg'
+import electoronIcon from '../../../Assets/devicons/electron-original.svg'
+import expressIcon from '../../../Assets/devicons/express-original.svg'
+import gitIcon from '../../../Assets/devicons/git-original.svg'
+import handlebarsIcon from '../../../Assets/devicons/handlebars-original.svg'
+import htmlIcon from '../../../Assets/devicons/html5-original.svg'
+import jsIcon from '../../../Assets/devicons/javascript-original.svg'
+import jqueryIcon from '../../../Assets/devicons/jquery-original-wordmark.svg'
+import mochaIcon from '../../../Assets/devicons/mocha-plain.svg'
+import mongodbIcon from '../../../Assets/devicons/mongodb-original-wordmark.svg'
+import mysqlIcon from '../../../Assets/devicons/mysql-original-wordmark.svg'
+import nodeIcon from '../../../Assets/devicons/nodejs-original.svg'
+import postgresIcon from '../../../Assets/devicons/postgresql-original-wordmark.svg'
+import pythonIcon from '../../../Assets/devicons/python-original.svg'
+import reactIcon from '../../../Assets/devicons/react-original-wordmark.svg'
+import sassIcon from '../../../Assets/devicons/sass-original.svg'
+import sequalizeIcon from '../../../Assets/devicons/sequelize-original.svg'
+
+
+
+
 
 // use this dumb component to organize import and send off a full list of svgs to use for develop.jsx 
 
 
-function devLanguages() {
+function DevLanguages() {
     return (
-        <div>
+        <div className='devLanguagesIcons'>
+            <div className="col-xs-12">
+            <img className='languageIcon' alt='html icon' src={htmlIcon} />
+            <img className='languageIcon' alt='css icon' src={cssIcon} />
+            <img className='languageIcon' alt='js icon' src={jsIcon} />
+            <img className='languageIcon' alt='sass icon' src={sassIcon} />
 
+            </div>
+            <div className="col-xs-12">
+            <img className='languageIcon' alt='handle bars icon' src={handlebarsIcon} />
+            <img className='languageIcon' alt='jquery icon' src={jqueryIcon} />
+            <img className='languageIcon' alt='node icon' src={nodeIcon} />
+            <img className='languageIcon' alt='express icon' src={expressIcon} />
+
+            </div>
+            <div className="col-xs-12">
+            <img className='languageIcon' alt='mongo db icon' src={mongodbIcon} />
+            <img className='languageIcon' alt='mysql icon' src={mysqlIcon} />
+            <img className='languageIcon' alt='postgresql icon' src={postgresIcon} />
+            <img className='languageIcon' alt='sequalize icon' src={sequalizeIcon} />
+
+            </div>
+            <div className="col-xs-12">
+            <img className='languageIcon' alt='docker icon' src={dockerIcon} />
+            <img className='languageIcon' alt='csharp icon' src={csharpIcon} />
+            <img className='languageIcon' alt='python icon' src={pythonIcon} />
+            <img className='languageIcon' alt='react icon' src={reactIcon} />
+
+            </div>
+            <div className="col-xs-12">
+            <img className='languageIcon' alt='git icon' src={gitIcon} />
+            <img className='languageIcon' alt='electorn icon' src={electoronIcon} />
+            <img className='languageIcon' alt='mocha icon' src={mochaIcon} />
+
+            </div>
         </div>
-    )
+    );
 }
+
+DevLanguages.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default (DevLanguages);
