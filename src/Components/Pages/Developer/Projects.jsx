d61f374966abd23343cda7fdba175cb1b1e05bee
Ian Mitchell <ianmitch90@gmail.com>
2018-08-12 17:17:47 -0500

    MVP done

--- src/Components/Pages/Developer/Projects.jsx
+++ src/Components/Pages/Developer/Projects.jsx
@@ -1,11 +1,84 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import shakingImg from '../../../Assets/images/whatsshaking.jpeg'
+import soundcloudImg from '../../../Assets/images/soundcloud.jpeg'
 
-const Projects = () => {
+
+const styles = {
+  card: {
+    maxWidth: 345,
+    margin: '5px'
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+};
+
+function Projects(props) {
+  const { classes } = props;
   return (
-    <div className='projectsContainer'>
-      all the things projects
+    <div className='projectCardContainer'>
+      <Card className={classes.card}>
+        <CardMedia
+          className={classes.media}
+          image={shakingImg}
+          title="What's Shaking?"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="headline" component="h2">
+            Whats Shaking?
+          </Typography>
+          <Typography>
+            <p>An React app that uses data from the U.S. Geological Survay API to show you the most recent seismic activity.</p>
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" color="primary" target="_blank" rel="noopener noreferrer" href='https://github.com/ianmitch90/reactearthquake'>
+            Git Hub
+          </Button>
+          <Button size="small" color="primary" target="_blank" rel="noopener noreferrer" href='https://ianmitch90.github.io/reactearthquake/'>
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+      <Card className={classes.card}>
+        <CardMedia
+          className={classes.media}
+          image={soundcloudImg}
+          title="CloudSound Search"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="headline" component="h2">
+            CloudSound Search
+          </Typography>
+          <Typography>
+            <p>This project uses the SoundCloud API to search for music</p>
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" color="primary" target="_blank" rel="noopener noreferrer" href= 'https://github.com/ianmitch90/soundcloud'>
+            Git Hub
+          </Button>
+          <Button size="small" color="primary" target="_blank" rel="noopener noreferrer" href= 'https://ianmitch90.github.io/soundcloud/'>
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+
     </div>
-  )
+  );
 }
 
-export default Projects
+Projects.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Projects);
--- src/Components/Pages/Social/Social.jsx
+++ src/Components/Pages/Social/Social.jsx
@@ -1,13 +1,39 @@
 import React, { Component } from 'react'
+import Button from '@material-ui/core/Button';
 import './social.css'
+import Twitter from './svg/twitter.svg'
+import Twitch from './svg/twitch.svg'
+import Medium from './svg/medium.svg'
+import Github from './svg/github.svg'
+
 export class Social extends Component {
   render() {
     return (
       <div className='socialContainer topographyGradient'>
-        <div className='socialButtonsContainer'>
-          <div className='socialHeroImage'>
-            <div className='socialHeroText'>
-                <h1>Follow <br/> My <br/> Journey!</h1>
+       <div className='socialContent'>
+          <div className='socialButtonsContainer'>
+            <div className='socialHeroImage'>
+              <div className='socialHeroText'>
+                  <h1>Follow <br/> My <br/> Journey!</h1>
+              </div>
+            </div>
+          </div>
+          <div className='socialIconsContainer'>
+            <div className='socialRow'>
+              <Button href='https://twitter.com/TheShadyGaymer' className='socialIcons' color='primary' variant='fab' target="_blank" rel="noopener noreferrer">
+                  <img src={Twitter}/>
+                </Button>
+              <Button href='https://www.twitch.tv/theshadygaymer' className='socialIcons' color='primary' variant='fab' target="_blank" rel="noopener noreferrer">
+                  <img src={Twitch}/>
+                </Button>
+            </div>
+            <div className='socialRow'>
+              <Button href='https://medium.com/@ianmitch90' className='socialIcons' color='primary' variant='fab' target="_blank" rel="noopener noreferrer">
+                  <img src={Medium}/>
+                </Button>
+              <Button href='https://github.com/ianmitch90/' className='socialIcons' color='primary' variant='fab' target="_blank" rel="noopener noreferrer">
+                  <img src={Github}/>
+                </Button>
             </div>
           </div>
         </div>
