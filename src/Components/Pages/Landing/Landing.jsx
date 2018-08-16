4fbbdf95dd2b264a6a2596884aa15881da099547
Ian Mitchell <ianmitch90@gmail.com>
2018-08-15 22:20:34 -0500

    added gradient back to line
    the build process does not like deg's on linear gradients to avoid this build error i just used right

--- src/Components/Pages/Landing/Landing.jsx
+++ src/Components/Pages/Landing/Landing.jsx
@@ -5,7 +5,7 @@ import { CallToSwipe, Trailing } from './CTA/CTAnimation'
 
 // ok so each layer has its own section, points of intrest lie in how im handling my props in Page. more intrestingly is that im using a terinary operator on line 34 to detect if a map generated has an empty string not to render the CallToSwipe component that was imported from CTAnimation
 
-const Page = ({ offset, caption, first, second, third, fourth, fifth, sixth, bgTitle, gradient, onClick, isWelcomePanel }) => (
+const Page = ({ offset, caption, first, second, third, fourth, fifth, sixth, bgTitle, gradient, onClick, isWelcomePanel, gradientLine }) => (
 
   <React.Fragment>
     <Parallax.Layer offset={offset} speed={0.2} onClick={onClick}>
@@ -23,7 +23,7 @@ const Page = ({ offset, caption, first, second, third, fourth, fifth, sixth, bgT
     <Parallax.Layer className="text landingSlideHeader" offset={offset} speed={0.4} >
       <span>
         <p style={{ fontSize: 20 }}>{caption}</p>
-        <div className={`stripe ${gradient}`} />
+        <div className={`stripe ${gradientLine}`} />
         <p>{first}</p>
         <p>{second}</p>
         <p>{third}</p>
@@ -69,15 +69,15 @@ export class Landing extends Component {
             <ParallaxLayer offset={1}>
               <Parallax className="container" ref="parallax" pages={5} horizontal scrolling={true}>
 
-                <Page offset={0} gradient="pink" caption="Who I Am" first='Developer' second="&" third="Designer" fourth="-" fifth='to begin:' sixth='swipe or tap' onClick={() => this.scroll(1)} bgTitle='Hey there' isWelcomePanel='things' />
+                <Page offset={0} gradientLine='pinkAccent' gradient="pink" caption="Who I Am" first='Developer' second="&" third="Designer" fourth="-" fifth='to begin:' sixth='swipe or tap' onClick={() => this.scroll(1)} bgTitle='Hey there' isWelcomePanel='things' />
 
-                <Page offset={1} gradient="teal" caption="What I Do" first="" second="Development" third="Education" fourth="Freelance" onClick={() => this.scroll(2)} bgTitle='Career' />
+                <Page offset={1} gradientLine='tealAccent' gradient="teal" caption="What I Do" first="" second="Development" third="Education" fourth="Freelance" onClick={() => this.scroll(2)} bgTitle='Career' />
 
-                <Page offset={2} gradient="tomato" caption="Web Development Specialties" first="Jr Full Stack Dev" second="JS/HTML/CSS/C#" third="Mongo/PostSQL" fourth="React/Redux/MERN" onClick={() => this.scroll(3)} bgTitle='Skills' />
+                <Page offset={2} gradientLine='tomatoAccent' gradient="tomato" caption="Web Development Specialties" first="Jr Full Stack Dev" second="JS/HTML/CSS/C#" third="Mongo/PostSQL" fourth="React/Redux/MERN" onClick={() => this.scroll(3)} bgTitle='Skills' />
 
-                <Page offset={3} gradient="algae" caption="Education" first="University" second="of Houston" third="Computer Science " fourth="- Current" fifth="The Iron Yard" sixth="Full Stack - 2017" onClick={() => this.scroll(4)} bgTitle='School' />
+                <Page offset={3} gradientLine='algaeAccent' gradient="algae" caption="Education" first="University" second="of Houston" third="Computer Science " fourth="- Current" fifth="The Iron Yard" sixth="Full Stack - 2017" onClick={() => this.scroll(4)} bgTitle='School' />
 
-                <Page offset={4} gradient="lavender" caption="Contact Me" first="Visit my" second="Contact Page" third="For More" fourth="Information!" onClick={() => this.scroll(0)} bgTitle='Explore!' />
+                <Page offset={4} gradientLine='lavenderAccent' gradient="lavender" caption="Contact Me" first="Visit my" second="Contact Page" third="For More" fourth="Information!" onClick={() => this.scroll(0)} bgTitle='Explore!' />
 
               </Parallax>
             </ParallaxLayer>
